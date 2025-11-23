dc239ac0398622fd18fbe02dc9e71174b07e320d
Hassaan Raza <hassaanRaza71@gmail.com>
2025-11-23 16:17:27 +0500

    Implement room assignment and patient addition functions
    Added functions to assign and free rooms, and to add patients.

--- main.c
+++ main.c
@@ -36,3 +36,28 @@ void show_rooms() {
     }
 }
 
+int assign_room() {
+    for (int i = 0; i < MAX_ROOMS; i++) {
+        if (rooms[i] == 0) {
+            rooms[i] = 1;
+            return i + 1;
+        }
+    }
+    return -1;
+}
+
+void free_room(int room_no) {
+    if (room_no > 0 && room_no <= MAX_ROOMS) {
+        rooms[room_no - 1] = 0;
+    }
+}
+
+void add_patient() {
+    if (patient_count >= MAX_PATIENTS) {
+        printf("Patient list full!\n");
+        return;
+    }
+
+    Patient p;
+    p.id = patient_count + 1;
+
