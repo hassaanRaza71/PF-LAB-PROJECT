5a023b977692baec68580455d676af51742800e3
Hassaan Raza <hassaanRaza71@gmail.com>
2025-11-23 16:14:13 +0500

    Define Patient struct for patient management
    Added Patient struct to manage patient data.

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_PATIENTS 100
+#define MAX_ROOMS 10
+
+typedef struct {
+    int id;
+    char name[50];
+    char gender[10];
+    int age;
+    char disease[50];
+    int room_no;
+} Patient;
 
